49dd33d9acc4adea1a83ddeedf80cef42e366050
domendraKS <domendrakumarsahu542@gmail.com>
2023-09-25 14:50:44 +0530

    user api started

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import "dotenv/config";
 import mongoose from "mongoose";
+import userRouter from "./routers/user.route.js";
 
 const app = express();
 
 const Port = process.env.PORT;
+
+app.use('/auth/user', userRouter)
 
 const DB_CONN = () =>{
     mongoose.connect(process.env.DB_URL).then(() => {
