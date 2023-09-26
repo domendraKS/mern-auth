2a2e269a3327be4275e359b09527971042fe2261
domendraKS <domendrakumarsahu542@gmail.com>
2023-09-26 11:19:18 +0530

    Header complete

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom"
 
 function Header() {
   return (
-    <div >
-      <div className='d-flex justify-content-around'>
-        <h1>Auth App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Sign-Up</li>
+    <div className='bg-secondary border-bottom border-primary headerDiv'>
+      <div className='d-flex justify-content-between align-items-center px-3'>
+        <h1 className='fw-bold'>Auth App</h1>
+        <ul className='d-flex gap-4 headerUl'>
+        <Link to="/"><li>Home</li></Link>
+        <Link to="/about"><li>About</li></Link>
+        <Link to="/signUp"><li>Sign-Up</li></Link>
         </ul>
       </div>
     </div>
