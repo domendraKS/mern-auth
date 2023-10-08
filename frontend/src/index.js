c7c1f5c062ff290397e2465bc5f8aaed1e65773c
domendraKS <domendrakumarsahu542@gmail.com>
2023-10-08 17:20:22 +0530

    Redux-persist done for LocalStorage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,14 @@ import "./index.css";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
-import { Store } from "./redux/Store";
+import { Store, persistor } from "./redux/Store";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={Store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- frontend/src/redux/Store.js
+++ frontend/src/redux/Store.js
@@ -1,6 +1,20 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import useReducer from "./user/userSlice.js";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({ user: useReducer });
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const Store = configureStore({
-  reducer: { user: useReducer },
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(Store);
