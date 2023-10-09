8afbd8862b2dbad0a7895c6d228be06849a529d6
domendraKS <domendrakumarsahu542@gmail.com>
2023-10-09 21:46:57 +0530

    Update header component and make profile private

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,25 @@
-import './App.css';
-import {Route, BrowserRouter as Router, Routes} from "react-router-dom"
-import Layout from './layout/Layout';
-import Home from './pages/Home';
-import SignUp from './pages/SignUp';
-import About from './pages/About';
-import SignIn from './pages/SignIn';
-import Profile from './pages/Profile';
+import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Layout from "./layout/Layout";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/signUp' element={<SignUp />} />
-          <Route path='/signIn' element={<SignIn />} />
-          <Route path='/profile' element={<Profile />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/signIn" element={<SignIn />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
         </Route>
       </Routes>
     </Router>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,19 +1,44 @@
-import React from 'react';
-import { Link } from "react-router-dom"
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+  console.log(currentUser);
   return (
-    <div className='bg-dark-subtle border-bottom border-primary headerDiv'>
-      <div className='d-flex justify-content-between align-items-center px-3'>
-        <h1 className='fw-bold'><Link to="/" className='headerLogo'>Auth App</Link></h1>
-        <ul className='d-flex gap-4 headerUl'>
-        <Link to="/"><li>Home</li></Link>
-        <Link to="/about"><li>About</li></Link>
-        <Link to="/signUp"><li>Sign-Up</li></Link>
+    <div className="bg-dark-subtle border-bottom border-primary headerDiv">
+      <div className="d-flex justify-content-between align-items-center px-3">
+        <h1 className="fw-bold">
+          <Link to="/" className="headerLogo">
+            Auth App
+          </Link>
+        </h1>
+        <ul className="d-flex gap-4 headerUl">
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+          <Link to="/profile">
+            {currentUser ? (
+              <>
+                <img
+                  src={currentUser.profilePicture}
+                  alt="Profile"
+                  width={50}
+                  height={50}
+                  className="rounded-circle object-fit-cover"
+                />
+              </>
+            ) : (
+              <li>Sign-Up</li>
+            )}
+          </Link>
         </ul>
       </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
